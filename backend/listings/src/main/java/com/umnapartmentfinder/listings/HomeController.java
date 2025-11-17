5f16eba29656a6fe55047a7d77aaad2ad7bc1d19
Aaron Ma <aaronma2006@gmail.com>
2025-11-17 17:40:19 -0600

    more initialization
    more initial setup, localhost test working

--- backend/listings/src/main/java/com/umnapartmentfinder/listings/HomeController.java
+++ backend/listings/src/main/java/com/umnapartmentfinder/listings/HomeController.java
@@ -1,5 +1,15 @@
 package com.umnapartmentfinder.listings;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+    
+    // "/" represents the root of website
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
     
 }
