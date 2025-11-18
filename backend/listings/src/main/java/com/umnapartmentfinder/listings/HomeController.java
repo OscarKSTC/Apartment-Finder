f4f8951bb21a0bdbfdbb6f9f359a72f533ee6548
Aaron Ma <aaronma2006@gmail.com>
2025-11-17 19:06:26 -0600

    minor additions

--- backend/listings/src/main/java/com/umnapartmentfinder/listings/HomeController.java
+++ backend/listings/src/main/java/com/umnapartmentfinder/listings/HomeController.java
@@ -1,14 +1,19 @@
 package com.umnapartmentfinder.listings;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+    
     
     // "/" represents the root of website
     @RequestMapping("/")
     public String index() {
+        System.out.println("appName: " + appName);
         return "index.html";
     }
     
